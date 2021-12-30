b1f047f4a02448aeccfe4c5ed774e901ac7b2ec0
David Day <david.day@utexas.edu>
2021-12-30 17:44:06 -0600

    clear out main function

--- src/main.c
+++ src/main.c
@@ -2,17 +2,6 @@
 #include <string.h>
 
 int main(void){
-    uint8_t x[] = {1,2,3};
-    uint8_t y[] = {0,0,0};
-
-    /* always works */
-    memcpy(y, x, 3);
-
-    /* doesn't always work */
-    uint8_t *xptr = x;
-    uint8_t *yptr = y;
-    memcpy(yptr, xptr, 3);
-
     while (1)
         ;
 }
