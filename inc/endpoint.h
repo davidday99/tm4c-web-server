d3fe3a5961b582e1ae83a688bcd469b350c139ca
David Day <david.day@utexas.edu>
2022-02-11 03:27:39 -0600

    create endpoint structure and implement basic webserver loop

---
+++ inc/endpoint.h
@@ -0,0 +1,15 @@
+#ifndef _ENDPOINT_H_
+#define _ENDPOINT_H_
+
+#include <stdint.h>
+
+struct endpoint {
+    char *path;
+    void (*GET)();
+    void (*HEAD)();
+    void (*POST)();
+};
+
+const struct endpoint *get_endpoint(char *path, struct endpoint epts[], uint8_t eptcount);
+
+#endif /* _ENDPOINT_H_ */
--- src/webserver.c
+++ src/webserver.c
@@ -1,11 +1,25 @@
 #include <stdint.h>
 #include "webserver.h"
+#include "http1_0.h"
+#include "endpoint.h"
+
+#define REQUEST_BUF_LEN 2000
 
 static uint8_t WEBSERVER_RUNNING;
 
+const struct endpoint ENDPOINTS[] = {
+    {.path = "/", .GET = 0, .HEAD = 0, .POST = 0},
+    {.path = "/about", .GET = 0, .HEAD = 0, .POST = 0},
+};
+
+struct socket *HTTP_SOCKET;
+
 static void webserver_run(void);
+static webserver_serve(struct http_request_message *req);
 
 void webserver_init() {
+    HTTP_SOCKET = socket_init(SOCKTYPE_TCP);
+    socket_bind(HTTP_SOCKET, 80);
     return;
 }
 
@@ -15,10 +29,43 @@ void webserver_start() {
 }
 
 static void webserver_run(void) {
+    uint8_t buf[REQUEST_BUF_LEN];
+    struct http_request_message req;
     while (WEBSERVER_RUNNING) {
-        // accept client
-        // read http request
-        // serve request
-        // close connection
+        socket_accept(HTTP_SOCKET);
+        socket_read(HTTP_SOCKET, buf, 1000);
+        http_parse_request(buf, &req);
+        webserver_serve(&req);
+        //socket_close(HTTP_SOCKET);
+    }
+}
+
+static webserver_serve(struct http_request_message *req) {
+    struct endpoint *ep = get_endpoint(req->uri, ENDPOINTS, sizeof(ENDPOINTS));
+    void (*method)();
+
+    if (ep == 0) {
+        // return 404
+    }
+
+    switch (req->method) {
+        GET:
+            method = ep->GET;
+            break;
+        HEAD:
+            method = ep->HEAD;
+            break;
+        POST:
+            method = ep->POST;
+            break;
+        default:
+            // return unknown method error
+            break;
+    }
+
+    if (method != 0) {
+        method(req);
+    } else {
+        // return method not allowed
     }
 }
