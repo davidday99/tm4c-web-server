af39a664bbaad9e917598cca8656d8b2f76812a7
David Day <david.day@utexas.edu>
2022-02-20 20:41:08 -0600

    return NULL if no matching endpoint found

--- src/endpoint.c
+++ src/endpoint.c
@@ -2,8 +2,9 @@
 #include "endpoint.h"
 #include "string.h"
 
-const struct endpoint *get_endpoint(char *path, struct endpoint epts[], uint8_t eptcount) {
+const struct endpoint *get_endpoint(char *path, const struct endpoint epts[], uint8_t eptcount) {
     for (uint8_t i = 0; i < eptcount; i++)
         if (strcmp(path, epts[i].path) == 0)
             return &epts[i];
+    return 0;
 }
